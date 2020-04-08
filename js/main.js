8c6bd708bd1449b375f5be66bac78afe64a7eb63
kudashovdanila <48285878+kudashovdanila@users.noreply.github.com>
2020-04-08 17:29:12 +0700

    Готово

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
 $(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+  });
+
+  $(".reg-link").click(function (e) {
+    e.preventDefault();
+    $(".reg-modal").fadeIn();
+  });
+
+  $(".log-link").click(function (e) {
+    e.preventDefault();
+    $(".log-modal").fadeIn();
+  });
+
+  $(".reg-close").click(function (e) {
+    e.preventDefault();
+    $(".reg-modal").fadeOut();
+  });
+
+  $(".log-close").click(function (e) {
+    e.preventDefault();
+    $(".log-modal").fadeOut();
+  });
+
+  $(".reged-yet").click(function (e) {
+    e.preventDefault();
+    $(".reg-modal").fadeOut();
+  });
+
+  $(".recovery-link").click(function (e) {
+    e.preventDefault();
+    $(".recovery-modal").fadeIn();
+    $(".log-modal").fadeOut();
+  });
+
+  $(".recovery-close").click(function (e) {
+    e.preventDefault();
+    $(".recovery-modal").fadeOut();
+  });
+
+
   var app = document.getElementById('type-writer-message');
 
   var typewriter = new Typewriter(app, {
@@ -9,6 +54,29 @@ $(document).ready(function () {
     .pauseFor(1000)
     .deleteAll()
     .typeString('Я цифровой ассистент таргетолога!')
-    .pauseFor(1000)
     .start();
+
+  $('.features-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    dots: true,
+    slidesToScroll: 3,
+    responsive: [{
+        breakpoint: 993,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 632,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
